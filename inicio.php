29d56e3aaf3022f8be51be85b2a89dea77fd28cb
Ecodave <37601296+Ecodave@users.noreply.github.com>
2022-02-15 18:16:08 -0500

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styleindex.css"/>
+    <link rel="icon" type="image/icon" href="img/iconautoglob.ico">
+    <link rel="javaScript" href="js/index.js"/>
+    <title>Administración Auto Glob</title>
+</head>
+<body>
+    <header>
+        <div id="titulo">
+            <h1>AUTO GLOB</h1>
+        </div>
+    </header>
+    <div id="espacioregistro">
+        <iframe id="espreg" src="cuadros/registro.php" name="iframeregistro"
+                title="Registro, introducir la informacion del cliente y el vehiculo"></iframe>
+    </div>
+<!----------------------------------------------------------------------------------->
+    <div id="paid">
+        <iframe id="paid1" src="cuadros/paid.php"v name="iframepaid"
+                title="Pagos, introducir el dinero de la reparacion y las cuotas"></iframe>
+    </div>
+<!----------------------------------------------------------------------------------->
+    <div id="footer">
+        <iframe id="footer1" src="cuadros/footer.php" name="iframefooter"
+                title="banner final de la pagina que contiene informacion 
+                        de contacto, mapa e imagenes publicitarias"></iframe>
+    </div>
+</body>
+</html>
